e357f69405dee8411bb50d2987a239ea649c61d8
JingMike <Jing.Mike@qq.com>
2018-12-09 23:09:01 +0800

    V0.03: 2018.12.09, Phone information page parsed for the general part, needs detailed parse.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # SuNingSpider
 V0.00: 2018.12.05, repository initialization.
 V0.01: 2018.12.05, GitHub test.
+V0.02: 2018.12.05, List parsed, Phone URL ready.
+V0.03: 2018.12.09, Phone information page parsed for the general part, needs detailed parse.
--- main.py
+++ main.py
@@ -7,18 +7,21 @@ __author__ = "Jing Mike"
 
 import requests
 import os
+import re
 import time
 import threading
 from bs4 import BeautifulSoup
 
-def downloadlistpage(url):
+
+def downloadpage(url):
     '''
     download list page
     '''
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0"}
     contents = requests.get(url, headers=headers)
-    contents.encoding = 'gb2312'
+    contents.encoding = 'utf-8'
     return contents.text
+
 
 def parselist(pagecontents):
     '''
@@ -32,20 +35,47 @@ def parselist(pagecontents):
         phoneurl = tag.get('href')
         phonetitle = tag.get_text()
         phonelist.append(phoneurl)
+        break;
     return phonelist
+
 
 def getphoneinfo(phoneurl):
     phoneurl = 'https:' + phoneurl
-    print(phoneurl)
+    # url = 'https://product.suning.com/0000000000/690128134.html'
+    print('URL: ' + phoneurl)
+    phonepage = downloadpage(phoneurl)
+    soup = BeautifulSoup(phonepage, 'html.parser')
+    # seller
+    sectionseller = soup.find('div', class_="ds-dianzhao ds-bgnone")
+    print('sectionseller')
+    print(sectionseller)
+    # title
+    sectiontitle = soup.find('div', class_="proinfo-title")  # .find_all(['itemDisplayName', 'promotionDesc'])
+    print('sectiontitle')
+    print(sectiontitle)
+    # price
+    sectionprice = soup.find('div', class_="proinfo-focus clearfix")  # .find_all(['price-sn', 'price-promo'])
+    print('sectionprice')
+    print(sectionprice)
+    '''
+    for i in sectionprice:
+        detail = i.find('span', attrs={'class': 'mainprice'}).get_text()
+        print(detail)
+    '''
+    # parameter
+    sectionparameter = soup.find('div', class_="tzm")  # .find_all('colorItemList')
+    print('sectionparameter')
+    print(sectionparameter)
+
 
 def main():
     # iPhone sold by SuNing.
     url = 'https://search.suning.com/iphone/&ci=20006&hf=brand_Name_FacetAll:Apple&sc=0&ct=1&snyp=&st=0#second-filter'
-    listpage = downloadlistpage(url)
+    listpage = downloadpage(url)
     phonelist = parselist(listpage)
     for phone in phonelist:
         getphoneinfo(phone)
 
+
 if __name__ == '__main__':
     main()
-
